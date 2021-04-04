ad8f8068d46a8718a9ffa7add8a10a813944b0e8
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-04-04 18:44:13 -0400

    added  auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   has_many :reviews
 
   has_secure_password 
+  validates :email, presence: true 
   validates :email, uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true 
+  validates :last_name, presence: true 
+  validates :password, length: { minimum: 8 }
+
 
 end
--- config/application.rb
+++ config/application.rb
@@ -29,9 +29,13 @@ module VarreAppBackend
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    config.middleware.use ActionDispatch::Cookies    
+    config.middleware.use ActionDispatch::Session::CookieStore
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    config.api_only = false 
   end
 end
+
+
