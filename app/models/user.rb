caca338a1bef4e2b3249f97e528a57fd8cd2ed3a
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-04-16 08:07:17 -0400

    added custom routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,8 @@ class User < ApplicationRecord
   has_many :reviews
 
   has_secure_password 
-  validates :email, presence: true 
-  validates :email, uniqueness: { case_sensitive: false }
-  validates :first_name, presence: true 
-  validates :last_name, presence: true 
+  validates :email, :password, :first_name, :last_name, presence: true 
+  validates :email, uniqueness: true 
   validates :password, length: { minimum: 8 }
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do 
+    namespace :v1 do 
+      post "/users", to: "users#create"
+      get "/users", to: "users#index"
+      post "/login", to: "auth#login"
+      get "/authlogin", to: "users#autologin"
+    end
+  end
+  
+
   resources :users, only: [:create, :index, :show]
   resources :carts
   resources :items
